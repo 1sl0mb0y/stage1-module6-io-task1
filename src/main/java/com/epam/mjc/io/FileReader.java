96645a46da0fb7cab1175d469078aaf6cf5b5f4b
1sl0mb0y <eU@d9K3UtF@zUEc>
2023-11-12 15:53:30 +0500

    v2

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,17 +1,16 @@
 package com.epam.mjc.io;
 
 import java.io.*;
-import java.nio.file.Path;
-import java.util.HashMap;
+v2import java.util.HashMap;
 import java.util.Map;
 
 
 public class FileReader {
 
     public static Profile getDataFromFile(File file) {
-        try {
+        try(BufferedReader reader = new BufferedReader(new java.io.FileReader(file.getName()))) {
             Map<String, String> profileMap = new HashMap<>();
-            BufferedReader reader = new BufferedReader(new java.io.FileReader(file.getName()));
+            ;
             String line;
             while ((line = reader.readLine()) != null) {
                 profileMap.put(line.split(" ")[0], line.split(" ")[1]);
